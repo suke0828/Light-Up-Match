9db0f27f21ebabab7d84f765ca89874176a03267
suke0828 <mwbspa@gmail.com>
2024-03-30 19:38:46 +0900

    Add chains number
    チェーン数を扱う定数を追加した

--- src/types/constants.ts
+++ src/types/constants.ts
@@ -1,2 +1,5 @@
 export const BOARD_WIDTH = 8;
 export const LIGHTS_COLOR = ['blue', 'red', 'orange', 'green', 'yellow', 'purple'];
+export const THREE_CHAINS = 3;
+export const FOUR_CHAINS = 4;
+export const FIVE_CHAINS = 5;
