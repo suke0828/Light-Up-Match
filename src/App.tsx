10cd459ed366bf6b640a7a63526fbcdfef7b5bfb
suke0828 <mwbspa@gmail.com>
2024-04-03 19:30:24 +0900

    Remove useState
    state管理をboard.tsxで行うため削除した

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,12 @@
-import { useState } from 'react';
 import './App.css';
-import { TLight } from './features/lights/light.type';
 import { Board } from './features/boards/board';
 import { Title } from './features/title/title';
-import { initializeBoard } from './features/boards/initializeBoard';
 
 export const App = () => {
-  const [lights] = useState<TLight[]>(initializeBoard);
-
   return (
     <div className="app">
       <Title />
-      <Board lights={lights} />
+      <Board />
     </div>
   );
 };
