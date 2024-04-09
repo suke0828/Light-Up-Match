cab689a2e7aa6d642b23b0ca218621a06c753b53
suke0828 <mwbspa@gmail.com>
2024-04-09 13:24:48 +0900

    Change the process to be remove

--- src/features/chains/chainsBehavior.ts
+++ src/features/chains/chainsBehavior.ts
@@ -1,23 +1,23 @@
 import { TLight } from '../lights/light.type';
 import { moveDown } from './moveDown';
-import { checkColumnChains } from './checkColumnChains';
-import { checkRowChains } from './checkRowChains';
+import { removeChains } from './removeChains/removeChains';
 
 export const chainsBehavior = (currentLights: TLight[]): TLight[] => {
-  let nextLights = currentLights;
-  nextLights = checkColumnChains(nextLights);
-  nextLights = checkRowChains(nextLights);
+  let nextLights = [...currentLights];
+
+  nextLights = removeChains(nextLights);
 
   const isSamePrevAndCurrentLights = currentLights.every(
     (light, i) => light.color === nextLights[i].color && light.color !== ''
   );
+
+  moveDown(nextLights);
 
   // 現在のboardと前のboardのlightの配置が同じなら終了する
   if (isSamePrevAndCurrentLights) {
     return nextLights;
   }
 
-  nextLights = moveDown(nextLights);
   // 再帰関数
   return chainsBehavior(nextLights);
 };
