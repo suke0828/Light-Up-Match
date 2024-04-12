1074bd5c12da2636668910876865b7ce15ee9a2b
suke0828 <mwbspa@gmail.com>
2024-04-12 20:15:08 +0900

    Changed function name
    ファイルと関数の名前を変更した

--- src/features/chains/chainPossible.ts
+++ src/features/chains/chainPossible.ts
@@ -2,8 +2,8 @@ import { TLight } from '../lights/light.type';
 import { useEffect } from 'react';
 import { checkRightSide } from './checkChainPossible/checkRightSide';
 import { checkLeftSide } from './checkChainPossible/checkLeftSide';
-import { checkTopSide } from './checkChainPossible/checkTopSide';
-import { checkBottomSide } from './checkChainPossible/checkBottomSide';
+import { checkAbove } from './checkChainPossible/checkAbove';
+import { checkBelow } from './checkChainPossible/checkBelow';
 
 export const chainPossible = (
   lights: TLight[],
@@ -17,8 +17,8 @@ export const chainPossible = (
     const timer = setTimeout(() => {
       checkRightSide(newLights, setState);
       checkLeftSide(newLights, setState);
-      checkTopSide(newLights, setState);
-      checkBottomSide(newLights, setState);
+      checkAbove(newLights, setState);
+      checkBelow(newLights, setState);
     }, interval);
 
     // lightsが更新される度にtimerのミリ秒を0にリセットする
